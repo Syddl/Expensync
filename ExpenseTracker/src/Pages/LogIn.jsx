8d38efe04cd80d9d191b4d893f14663a3e2a30cf
Justine Jude Cuevas <justinecuevas19@gmail.com>
2025-03-08 16:00:44 +0800

    added login and setup design

--- ExpenseTracker/src/Pages/LogIn.jsx
+++ ExpenseTracker/src/Pages/LogIn.jsx
@@ -14,7 +14,7 @@ const LogIn = () => {
             <h1 className='font-bold text-3xl'>Welcome back</h1>
             <p className='text-lg'>Welcome back! Please enter your details.</p>
           </div>
-          <button className='bg-[#f2f0ef] w-20 h-10 flex justify-center items-center rounded-3xl border-solid border border-[#7c7b7b44] mb-5 cursor-pointer'><img src={Google} alt="google" className='w-5 h-5'/></button>
+          <button className='bg-[#f2f0ef] hover:bg-[#f4edff] w-20 h-10 flex justify-center items-center rounded-3xl border-solid border border-[#7c7b7b44] mb-5 cursor-pointer'><img src={Google} alt="google" className='w-5 h-5'/></button>
           <input type="text" required placeholder="Enter Username" className='bg-[#f2f0ef] w-85 h-10 pl-2 rounded-md mb-3 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500'/>
           <input type="text" required placeholder="Enter Password" className='bg-[#f2f0ef] w-85 h-10 pl-2 rounded-md mb-4 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500'/>
           <div className='flex w-80 justify-end'>
@@ -25,13 +25,17 @@ const LogIn = () => {
           <button className='w-85 h-10 rounded-3xl  bg-violet-600 text-white font-bold mb-5'>Log in</button>
           <p className='text-md'>Don't have an account?
             <Link to="/Signup">
-              <span className='text-violet-800 cursor-pointer'> Sign up</span>
+              <span className='text-[#7f5efd] cursor-pointer'> Sign up</span>
             </Link>
           </p>
         </form>
       </div>
-      <div className="text bg-amber-400 w-[40%]">
-
+      <div className="text bg-gradient-to-t from-purple-400 via-purple-400 to-violet-500  w-[40%] flex justify-center items-center">
+        <div className=" w-100">
+          <h1 className='font-bold text-white text-3xl text-center mb-'>"Track your expenses effortlessly and stay in control!"</h1>
+          <p className='text-center text-white'>Simplify your finances with ease! Log in to manage, track, and optimize your spending—all in one place.
+          Your financial journey starts here!</p>
+        </div>
       </div>
     </div>
   )
