1a832a184800455f488a28b23c38ae8670252c47
Justine Jude Cuevas <justinecuevas19@gmail.com>
2025-03-13 00:34:56 +0800

    added chart using MUI

--- ExpenseTracker/src/Pages/Home.jsx
+++ ExpenseTracker/src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import DashBoardHeader from "../components/DashBoardComponents/DashBoardHeader"
+import ChartsOverview from "../components/DashBoardComponents/ChartsOverview"
 
 const  Home = () => {
   return(
@@ -6,16 +7,27 @@ const  Home = () => {
      <DashBoardHeader />
      <main className=" h-auto w-100% pl-10 pr-10 ">
         <div className="upperContent flex gap-5 pt-5">
-          <div className="container h-40 bg-[#F8F9FA] rounded-xl"></div>
-          <div className="container h-40 bg-[#F8F9FA] rounded-xl"></div>
-          <div className="container h-40 bg-[#F8F9FA] rounded-xl"></div>
+          <div className="container h-30 bg-[#f1f1f1] rounded-xl pl-5 pt-5">
+            <p className="text-[#565857] text-md font-semibold font-[Montserrat] mb-1">Expenses</p>
+            <h1 className="text-[#00093c] text-5xl font-bold font-[Montserrat]">₱5,534.00</h1>
+          </div>
+          <div className="container h-30 bg-[#f1f1f1] rounded-xl pl-5 pt-5">
+            <p className="text-[#565857] text-md font-semibold font-[Montserrat] mb-1">Expenses</p>
+            <h1 className="text-[#00093c] text-5xl font-bold font-[Montserrat]">₱5,534.00</h1>
+          </div>
+          <div className="container h-30 bg-[#f1f1f1] rounded-xl pl-5 pt-5">
+            <p className="text-[#565857] text-md font-semibold font-[Montserrat] mb-1">Expenses</p>
+            <h1 className="text-[#00093c] text-5xl font-bold font-[Montserrat]">₱5,534.00</h1>
+          </div>
         </div>
         <div className="mainContent flex gap-5 mt-5">
-          <div className="container bg-[#F8F9FA] h-100 rounded-xl"></div>
-          <div className="container bg-[#F8F9FA] w-185 h-100 rounded-xl"></div>
+          <div className="container bg-[#f1f1f1] h-100 rounded-xl flex flex-col items-center">
+            <ChartsOverview />
+          </div>
+          <div className="container bg-[#f1f1f1] w-185 h-100 rounded-xl"></div>
         </div>
         <div className="lowerCotent">
-          <div className="container h-50 bg-[#F8F9FA] rounded-xl mt-5">
+          <div className="container h-50 bg-[#f1f1f1] rounded-xl mt-5">
             
           </div>
         </div>
